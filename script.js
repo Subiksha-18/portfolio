da0206e0e5b57a95b632a430e8d42694dac457ed
Subiksha K <subikshasubiksha872@gmail.com>
2026-05-12 09:32:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Contact Form Message
+
+document.getElementById("contactForm").addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    alert("Message Sent Successfully!");
+
+});
